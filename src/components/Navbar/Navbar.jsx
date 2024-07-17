c137922fc4e0f784110183c379d96f15aed41187
yasmina114 <yasminahedi0@gmail.com>
2024-07-17 21:33:38 +0100

    Second Commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Navbar.css'
+
+import logo from '../../assets/logo.png'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+        <li>TV Shows</li>
+        <li>Movies</li>
+        <li>New & Popular</li>
+        <li>My List</li>
+        <li>Browse by languages</li></ul>
+      </div>
+      <div className="navbar-right">
+
+      </div>
     </div>
   )
 }
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
 const Home = () => {
   return (
     <div className='home'>
-      
+  hello 
     </div>
   )
 }
-
+book
 export default Home
