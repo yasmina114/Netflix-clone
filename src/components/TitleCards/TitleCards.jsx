ec6f6de1707872ff5c311b90cfdd662717a4f06a
yasmina114 <yasminahedi0@gmail.com>
2024-07-20 19:07:56 +0100

    Fourth Commit

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,23 +1,24 @@
-import React,{useEffect, useRef} from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
-const cardsRef=useRef();
-
-useEffect(()=>{
-
-},[])
-
 const TitleCards = () => {
+  const cardsRef = useRef();
+
+  useEffect(() => {
+    // Your effect logic here
+  }, [])
+
   return (
     <div className='title-cards'>
       <h2>Popular on Netflix</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card,index)=>{ 
-        return <div className="card" key={index}>
-          <img src={card.img} alt="" />
-          <p>{card.name}</p>
-          </div>})}
+        {cards_data.map((card, index) => (
+          <div className="card" key={index}>
+            <img src={card.img} alt="" />
+            <p>{card.name}</p>
+          </div>
+        ))}
       </div>
     </div>
   )
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../Navbar/Navbar";
-import hero_banner from '../../assets/hero_banner.jpg'
-import hero_title from '../../assets/hero_title.png'
-import play_icon from '../../assets/Play_icons.png'
-import info_icon from '../../assets/Info_icon.png'
+import hero_banner from "../../../assets/hero_banner.jpg";
+import hero_title from "../../../assets/hero_title.png";
+import play_icon from "../../../assets/play_icon.png";
+import info_icon from "../../../assets/info_icon.png";
 import TitleCards from "../../TitleCards/TitleCards";
 
 
 const Home = () => {
   return (
     <div className="home">
-      <Navbar />pmg
+      <Navbar />
       <div className="hero">
         <img src={hero_banner} alt="" className="banner-img"/>
         <div className="hero-caption">
